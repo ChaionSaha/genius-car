7afba4c8975ec0906a1903c0899c68fbc3daea4d
Chaion Saha <sahachaion2000@gmail.com>
2023-03-14 20:30:35 +0600

    added react hook form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import AddService from './Pages/AddService/AddService';
 import Checkout from './Pages/Checkout/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
@@ -12,26 +13,40 @@ import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/checkout" element={
-          <RequireAuth>
-            <Checkout></Checkout>
-          </RequireAuth>
-        }></Route>
-        <Route path="*" element={<NotFound></NotFound>}></Route>
-      </Routes>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div>
+			<Header></Header>
+			<Routes>
+				<Route path='/' element={<Home></Home>}></Route>
+				<Route path='/home' element={<Home></Home>}></Route>
+				<Route
+					path='/service/:serviceId'
+					element={<ServiceDetail></ServiceDetail>}
+				></Route>
+				<Route path='/about' element={<About></About>}></Route>
+				<Route path='/login' element={<Login></Login>}></Route>
+				<Route path='/register' element={<Register></Register>}></Route>
+				<Route
+					path='/checkout'
+					element={
+						<RequireAuth>
+							<Checkout></Checkout>
+						</RequireAuth>
+					}
+				></Route>
+				<Route
+					path='/addservice'
+					element={
+						<RequireAuth>
+							<AddService></AddService>
+						</RequireAuth>
+					}
+				></Route>
+				<Route path='*' element={<NotFound></NotFound>}></Route>
+			</Routes>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import './ServiceDetails.css';
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
-    return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
-        </div>
-    );
+	const { serviceId } = useParams();
+	const [service, setService] = useState({});
+	useEffect(() => {
+		fetch(`http://localhost:5000/service/${serviceId}`)
+			.then((res) => res.json())
+			.then((data) => setService(data));
+	}, []);
+	return (
+		<div className='d-flex p-3 flex-column ml-auto justify-content-center align-items-center'>
+			<h2 className='mb-3'>{service.name}</h2>
+			<div className='border w-25 p-3 h-25'>
+				<img src={service.img} alt='service' className='w-100'></img>
+				<p className='text-center my-3'>{service.description}</p>
+				<div className='text-center d-flex align-items-center justify-content-center '>
+					<Link to='/checkout'>
+						<button className='btn btn-primary overflow-hidden checkout-btn'>
+							<span>Price: {service.price}</span>
+							<span>Proceed Checkout</span>
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ServiceDetail;
